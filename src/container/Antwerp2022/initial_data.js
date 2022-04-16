d9fd16932fc7df4332a535e0d1ea1870f2949e0b
ieb <1@wa.vg>
2022-04-16 22:43:28 +0800

    updated asia and american

--- src/container/Antwerp2022/initial_data.js
+++ src/container/Antwerp2022/initial_data.js
@@ -25,113 +25,97 @@
 */
 export const initialDataChallenger = [
   {
-    code: "am|09",
-    icon: "tba",
-    name: "AM #2",
+    code: "am/mibr",
+    name: "MIBR",
     seed: 9,
     description: "AM #2",
   },
   {
-    code: "am|10",
-    icon: "tba",
-    name: "AM #3",
+    code: "am/impe",
+    name: "Imperial Esports",
     seed: 10,
     description: "AM #3",
   },
   {
     code: "eu2|11",
-    icon: "tba",
     name: "EUB #4",
     seed: 11,
     description: "EUB #4",
   },
   {
     code: "eu1|12",
-    icon: "tba",
     name: "EUA #5",
     seed: 12,
     description: "EUA #5",
   },
   {
     code: "eu2|13",
-    icon: "tba",
     name: "EUB #5",
     seed: 13,
     description: "EUB #5",
   },
   {
     code: "eu1|14",
-    icon: "tba",
     name: "EUA #6",
     seed: 14,
     description: "EUA #6",
   },
   {
     code: "eu2|15",
-    icon: "tba",
     name: "EUB #6",
     seed: 15,
     description: "EUB #6",
   },
   {
     code: "eu2|16",
-    icon: "tba",
     name: "EUB #7",
     seed: 16,
     description: "EUB #7",
   },
   {
-    code: "asia|17",
-    icon: "tba",
-    name: "AP #1",
+    code: "ap/ihc",
+    name: "IHC Esports",
     seed: 17,
     description: "AP #1",
   },
   {
-    code: "asia|18",
-    icon: "tba",
-    name: "AP #2",
+    code: "ap/rng",
+    name: "Renegades",
     seed: 18,
     description: "AP #2",
   },
   {
-    code: "am|19",
-    icon: "tba",
-    name: "AM #4",
+    code: "am/col",
+    name: "Complexity Gaming",
     seed: 19,
     description: "AM #4",
   },
   {
-    code: "am|20",
-    icon: "tba",
-    name: "AM #5",
+    code: "am/liqu",
+    name: "Team Liquid",
     seed: 20,
     description: "AM #5",
   },
   {
-    code: "am|21",
-    icon: "tba",
-    name: "AM #6",
+    code: "am/9z",
+    name: "9z Team",
     seed: 21,
     description: "AM #6",
   },
   {
     code: "eu1|22",
-    icon: "tba",
     name: "EUA #7",
     seed: 22,
     description: "EUA #7",
   },
   {
     code: "eu1|23",
-    icon: "tba",
     name: "EUA #8",
     seed: 23,
     description: "EUA #8",
   },
   {
     code: "eu2|24",
-    icon: "tba",
     name: "EUB #8",
     seed: 24,
     description: "EUB #8",
@@ -142,56 +126,48 @@ export const initialDataChallenger = [
 export const initialDataLegends = [
   {
     code: "eu1|01",
-    icon: "tba",
     name: "EUA #1",
     seed: 1,
     description: "EUA #1",
   },
   {
     code: "eu2|02",
-    icon: "tba",
     name: "EUB #1",
     seed: 2,
     description: "EUB #1",
   },
   {
-    code: "am|03",
-    icon: "tba",
-    name: "AM #1",
+    code: "am/furi",
+    name: "FURIA Esports",
     seed: 3,
     description: "AM #1",
   },
   {
     code: "eu1|04",
-    icon: "tba",
     name: "EUA #2",
     seed: 4,
     description: "EUA #2",
   },
   {
     code: "eu2|05",
-    icon: "tba",
     name: "EUB #2",
     seed: 5,
     description: "EUB #2",
   },
   {
     code: "eu1|06",
-    icon: "tba",
     name: "EUA #3",
     seed: 6,
     description: "EUA #3",
   },
   {
     code: "eu2|07",
-    icon: "tba",
     name: "EUB #3",
     seed: 7,
     description: "EUB #3",
   },
   {
     code: "eu1|08",
-    icon: "tba",
     name: "EUA #4",
     seed: 8,
     description: "EUA #4",
@@ -200,98 +176,98 @@ export const initialDataLegends = [
 /*
 export const finalDataLegends = [
   {
-    "code": "nip",
-    "name": "Ninjas in Pyjamas",
+    code: "nip",
+    name: "Ninjas in Pyjamas",
     "seed": 1,
     "description": "EU #1"
   },
   {
-    "code": "furi",
-    "name": "FURIA",
+    code: "furi",
+    name: "FURIA",
     "seed": 2,
     "description": "NA #1"
   },
   {
-    "code": "navi",
-    "name": "Natus Vincere",
+    code: "navi",
+    name: "Natus Vincere",
     "seed": 3,
     "description": "CIS #1"
   },
   {
-    "code": "vita",
-    "name": "Vitality",
+    code: "vita",
+    name: "Vitality",
     "seed": 4,
     "description": "EU #2"
   },
   {
-    "code": "liq",
-    "name": "Liquid",
+    code: "liq",
+    name: "Liquid",
     "seed": 5,
     "description": "NA #2"
   },
   {
-    "code": "gamb",
-    "name": "Gambit",
+    code: "gamb",
+    name: "Gambit",
     "seed": 6,
     "description": "CIS #2"
   },
   {
-    "code": "g2",
-    "name": "G2",
+    code: "g2",
+    name: "G2",
     "seed": 7,
     "description": "EU #3"
   },
   {
-    "code": "evl",
-    "name": "Evil Genius",
+    code: "evl",
+    name: "Evil Genius",
     "seed": 8,
     "description": "NA #3"
   },
   {
-    "code": "faze",
-    "name": "FaZe Clan",
+    code: "faze",
+    name: "FaZe Clan",
     "seed": 9,
     "description": "0L, 3B, #24"
   },
   {
-    "code": "cope",
-    "name": "Copenhagen Flames",
+    code: "cope",
+    name: "Copenhagen Flames",
     "seed": 10,
     "description": "0L, 1B, #23"
   },
   {
-    "code": "ence",
-    "name": "ENCE",
+    code: "ence",
+    name: "ENCE",
     "seed": 11,
     "description": "1L, 0B, #12"
   },
   {
-    "code": "ent",
-    "name": "Entropiq",
+    code: "ent",
+    name: "Entropiq",
     "seed": 12,
     "description": "1L, 0B, #20"
   },
   {
-    "code": "vp",
-    "name": "Virtus.pro",
+    code: "vp",
+    name: "Virtus.pro",
     "seed": 13,
     "description": "1L, -1B, #22"
   },
   {
-    "code": "hero",
-    "name": "Heroic",
+    code: "hero",
+    name: "Heroic",
     "seed": 14,
     "description": "2L, 3B, #15"
   },
   {
-    "code": "astr",
-    "name": "Astralis",
+    code: "astr",
+    name: "Astralis",
     "seed": 15,
     "description": "2L, -1B, #10"
   },
   {
-    "code": "mouz",
-    "name": "MOUZ",
+    code: "mouz",
+    name: "MOUZ",
     "seed": 16,
     "description": "2L, -3B, #16"
   }
@@ -300,50 +276,50 @@ export const finalDataLegends = [
 
 export const finalDataChampions = [
   {
-    "code": "navi",
-    "name": "Natus Vincere",
+    code: "navi",
+    name: "Natus Vincere",
     "seed": 1,
     "description": "0L, 4B, #3"
   },
   {
-    "code": "g2",
-    "name": "G2",
+    code: "g2",
+    name: "G2",
     "seed": 2,
     "description": "0L, -3B, #7"
   },
   {
-    "code": "hero",
-    "name": "Heroic",
+    code: "hero",
+    name: "Heroic",
     "seed": 3,
     "description": "1L, 1B, #14"
   },
   {
-    "code": "gamb",
-    "name": "Gambit",
+    code: "gamb",
+    name: "Gambit",
     "seed": 4,
     "description": "1L, -2B, #6"
   },
   {
-    "code": "furi",
-    "name": "FURIA",
+    code: "furi",
+    name: "FURIA",
     "seed": 5,
     "description": "1L, -7B, #2"
   },
   {
-    "code": "vp",
-    "name": "Virtus.pro",
+    code: "vp",
+    name: "Virtus.pro",
     "seed": 6,
     "description": "2L, 3B, #13"
   },
   {
-    "code": "nip",
-    "name": "Ninjas in Pyjamas",
+    code: "nip",
+    name: "Ninjas in Pyjamas",
     "seed": 7,
     "description": "2L, 0B, #1"
   },
   {
-    "code": "vita",
-    "name": "Vitality",
+    code: "vita",
+    name: "Vitality",
     "seed": 8,
     "description": "2L, -3B, #4"
   }
