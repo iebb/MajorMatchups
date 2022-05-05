1e7d32cdff8ada32b20f2675c44c4b0d185cc457
ieb <1@wa.vg>
2022-05-05 22:41:03 +0800

    online pickems

--- src/container/Antwerp2022/index.js
+++ src/container/Antwerp2022/index.js
@@ -339,9 +339,11 @@ export default class Antwerp2022 extends React.PureComponent {
           <div className="title-container">
             <h1 className="title">PGL Antwerp Major 2022 Matchup Calculator</h1>
           </div>
-          <h3 style={{ color: 'yellow'}}>
-            Seeding updated based on rules from the <a href="https://counter-strike.net/csgo_major_supplemental_rulebook/#Final-Rankings-Major">Rulebook</a>.
-            Should be correct now.
+          <h3>
+            Seeding based on rules from the <a href="https://counter-strike.net/csgo_major_supplemental_rulebook/#Final-Rankings-Major">Rulebook</a>.
+          </h3>
+          <h3 style={{ color: 'yellow' }}>
+            *NEW* Do Pick'ems Online - <a href="https://pick.ieb.im/" target="_blank">https://pick.ieb.im/</a> (Experimental)
           </h3>
           <p>
             <a href="https://discord.gg/KYNbRYrZGe">
