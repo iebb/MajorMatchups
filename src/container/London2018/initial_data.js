2fa5d5568f7a81148036f47e203217473e6eb921
ieb <1@wa.vg>
2022-05-15 13:50:35 +0800

    remove

---
+++ src/container/London2018/initial_data.js
@@ -0,0 +1,101 @@
+// prettier-ignore
+
+export const initialDataChallenger = [
+  {
+    code: "gamb",
+    name: "Gambit Esports",
+    seed: 9,
+    description: "9th Boston",
+  },
+  {
+    code: "vega",
+    name: "Vega Squadron",
+    seed: 10,
+    description: "10th Boston",
+  },
+  {
+    code: "spc",
+    name: "Space Soldiers",
+    seed: 11,
+    description: "11th Boston",
+  },
+  {
+    code: "liq",
+    name: "Team Liquid",
+    seed: 12,
+    description: "12th Boston",
+  },
+  {
+    code: "big",
+    name: "BIG",
+    seed: 13,
+    description: "13th Boston",
+  },
+  {
+    code: "astr",
+    name: "Astralis",
+    seed: 14,
+    description: "14th Boston",
+  },
+  {
+    code: "nor",
+    name: "North",
+    seed: 15,
+    description: "15th Boston",
+  },
+  {
+    code: "vp",
+    name: "Virtus.pro",
+    seed: 16,
+    description: "16th Boston",
+  },
+  {
+    code: "nip",
+    name: "Ninjas in Pyjamas",
+    seed: 17,
+    description: "EU Minor #1",
+  },
+  {
+    code: "hlr",
+    name: "HellRaisers",
+    seed: 18,
+    description: "CIS Minor #1",
+  },
+  {
+    code: "col",
+    name: "compLexity Gaming",
+    seed: 19,
+    description: "AM Minor #1",
+  },
+  {
+    code: "ren",
+    name: "Renegades",
+    seed: 20,
+    description: "AP Minor #1",
+  },
+  {
+    code: "optc",
+    name: "OpTic Gaming",
+    seed: 21,
+    description: "EU Minor #1",
+  },
+  {
+    code: "rog",
+    name: "Rogue",
+    seed: 22,
+    description: "AM Minor #2",
+  },
+  {
+    code: "spir",
+    name: "Team Spirit",
+    seed: 23,
+    description: "CIS Minor #2",
+  },
+  {
+    code: "tyl",
+    name: "TYLOO",
+    seed: 24,
+    description: "AP Minor #2",
+  }
+];
+
