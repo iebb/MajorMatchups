0d6b01199305a7e73633a4723b6935fda5d58897
ieb <1@wa.vg>
2022-05-18 13:47:52 +0800

    fix

--- src/libs/common/formats/Knockout.js
+++ src/libs/common/formats/Knockout.js
@@ -24,8 +24,9 @@ export function Knockout(fromStage, toStage) {
       if (y.w - x.w) return y.w - x.w;
       if (!y._seed) y._seed = y.seed;
       if (!x._seed) x._seed = x.seed;
-      if (y._seed - x._seed) return y._seed - x._seed;
-
+      if (y.l < loseToEliminate) {
+        if (y._seed - x._seed) return y._seed - x._seed;
+      }
       return x.seed - y.seed;
     };
 
