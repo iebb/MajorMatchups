d4ed313dc1159bf7bef58c86d4a8e7565929e8a7
ieb <1@wa.vg>
2023-02-28 11:51:41 +0800

    Update index.js

--- src/container/Rio2022/index.js
+++ src/container/Rio2022/index.js
@@ -27,7 +27,7 @@ const TournamentLegends = 1;
 const TournamentChampions = 2;
 
 const teamLogo = (code) => `https://majors.im/images/rio2022/${code}_glitter_large.png`;
-const redirectLink = "https://cutt.ly/INjQYvS";
+const redirectLink = "https://cutt.ly/5Bikcss";
 
 
 const TournamentStages = [
