8f808d1a8d8d4a34ced0afa8f7ef17eb2eb0565a
ieb <1@wa.vg>
2024-01-21 21:13:49 +0800

    10th

--- src/container/CS2/2024/Copenhagen/Copenhagen2024RMR/index.js
+++ src/container/CS2/2024/Copenhagen/Copenhagen2024RMR/index.js
@@ -345,7 +345,7 @@ const Regions = [
           if (teamA.standing !== teamB.standing) {
             return teamA.standing - teamB.standing;
           }
-          if (teamA.buchholtz !== teamB.buchholtz) {
+          if (teamA.standing === 10 && (teamA.buchholtz !== teamB.buchholtz)) {
             return teamB.buchholtz - teamA.buchholtz;
           }
           if (teamA.group !== teamB.group) {
