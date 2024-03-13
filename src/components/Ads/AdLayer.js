8b7f86fea8ddcb2d7744d9f3c2abb8f05b66583d
ieb <1@wa.vg>
2024-03-13 19:25:25 +0800

    fix

--- src/components/Ads/AdLayer.js
+++ src/components/Ads/AdLayer.js
@@ -4,19 +4,19 @@ import GoogleAd from "./GoogleAd";
 export const BottomAdLayer = () => {
   const ad = window.config;
 
-
-  if (ad.adtype === "google") {
-    return (
-      <div className="bottom-desktop" style={{ maxHeight: 93 }}>
-        <GoogleAd
-          style={{display: 'block'}}
-          googleAdId="ca-pub-3253159471656308"
-          format="autorelaxed"
-          slot="1398483557"
-        />
-      </div>
-    )
-  }
+  //
+  // if (ad.adtype === "google") {
+  //   return (
+  //     <div className="bottom-desktop" style={{ maxHeight: 93 }}>
+  //       <GoogleAd
+  //         style={{display: 'block'}}
+  //         googleAdId="ca-pub-3253159471656308"
+  //         format="autorelaxed"
+  //         slot="1398483557"
+  //       />
+  //     </div>
+  //   )
+  // }
 
 
   if (ad.adtype === "custom") {
