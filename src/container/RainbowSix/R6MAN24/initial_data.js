ec84f3d5a7f5b6f23f6f0ca20303f69d76357925
ieb^ <jebwizoscar@hotmail.com>
2024-05-18 05:00:32 +0800

    fix

--- src/container/RainbowSix/R6MAN24/initial_data.js
+++ src/container/RainbowSix/R6MAN24/initial_data.js
@@ -2,67 +2,74 @@
 
 export const initialDataChallenger =  [
   {
-    "code": "bds",
-    "name": "Team BDS",
-    "icon": "https://img.majors.im/r6/man24/bds.png",
-    "seed": 1,
-    "description": "Seed #1"
-  },
-  {
     "code": "beas",
     "name": "beastcoast",
     "icon": "https://img.majors.im/r6/man24/beas.png",
-    "seed": 2,
-    "description": "Seed #2"
+    "seed": 1,
+    "description": "Seed #1"
   },
   {
     "code": "faze",
     "name": "FaZe Clan",
     "icon": "https://img.majors.im/r6/man24/faze.png",
+    "seed": 2,
+    "description": "Seed #2"
+  },
+  {
+    "code": "bds",
+    "name": "Team BDS",
+    "icon": "https://img.majors.im/r6/man24/bds.png",
     "seed": 3,
     "description": "Seed #3"
-  },
-  {
-    "code": "into",
-    "name": "Into The Breach",
-    "icon": "https://img.majors.im/r6/man24/into.png",
-    "seed": 4,
-    "description": "Seed #4"
-  },
-  {
-    "code": "m80",
-    "name": "M80",
-    "icon": "https://img.majors.im/r6/man24/m80.png",
-    "seed": 5,
-    "description": "Seed #5"
   },
   {
     "code": "e1",
     "name": "E1 Sports",
     "icon": "https://img.majors.im/r6/man24/e1.png",
-    "seed": 6,
-    "description": "Seed #6"
+    "seed": 4,
+    "description": "Seed #4"
   },
   {
-    "code": "scar",
-    "name": "SCARZ",
-    "icon": "https://img.majors.im/r6/man24/scar.png",
-    "seed": 7,
-    "description": "Seed #7"
+    "code": "into",
+    "name": "Into The Breach",
+    "icon": "https://img.majors.im/r6/man24/into.png",
+    "seed": 5,
+    "description": "Seed #5"
+  },
+  {
+    "code": "m80",
+    "name": "M80",
+    "icon": "https://img.majors.im/r6/man24/m80.png",
+    "seed": 6,
+    "description": "Seed #6"
   },
   {
     "code": "psg",
     "name": "PSG Talon",
     "icon": "https://img.majors.im/r6/man24/psg.png",
+    "seed": 7,
+    "description": "Seed #7"
+  },
+  {
+    "code": "scar",
+    "name": "SCARZ",
+    "icon": "https://img.majors.im/r6/man24/scar.png",
     "seed": 8,
     "description": "Seed #8"
+  },
+  {
+    "code": "dark",
+    "name": "DarkZero Esports",
+    "icon": "https://img.majors.im/r6/man24/dark.png",
+    "seed": 9,
+    "description": "Seed #9"
   },
   {
     "code": "secr",
     "name": "Team Secret",
     "icon": "https://img.majors.im/r6/man24/secr.png",
-    "seed": 9,
-    "description": "Seed #9"
+    "seed": 10,
+    "description": "Seed #10"
   },
   {
     "code": "furi",
@@ -72,13 +79,6 @@ export const initialDataChallenger =  [
     "description": "Seed #11"
   },
   {
-    "code": "dark",
-    "name": "DarkZero Esports",
-    "icon": "https://img.majors.im/r6/man24/dark.png",
-    "seed": 10,
-    "description": "Seed #10"
-  },
-  {
     "code": "blis",
     "name": "Team Bliss",
     "icon": "https://img.majors.im/r6/man24/blis.png",
@@ -86,28 +86,32 @@ export const initialDataChallenger =  [
     "description": "Seed #12"
   },
   {
+    "code": "spac",
+    "name": "Spacestation Gaming",
+    "icon": "https://img.majors.im/r6/man24/spac.png",
     "seed": 13,
-    "code": "t13",
-    "name": "Team 13",
-    "icon": "https://ui-avatars.com/api/?name=Team 13&background=random&length=3"
+    "description": "Seed #13"
   },
   {
+    "code": "liqu",
+    "name": "Team Liquid",
+    "icon": "https://img.majors.im/r6/man24/liqu.png",
     "seed": 14,
-    "code": "t14",
-    "name": "Team 14",
-    "icon": "https://ui-avatars.com/api/?name=Team 14&background=random&length=3"
+    "description": "Seed #14"
   },
   {
+    "code": "blee",
+    "name": "Bleed Esports",
+    "icon": "https://img.majors.im/r6/man24/blee.png",
     "seed": 15,
-    "code": "t15",
-    "name": "Team 15",
-    "icon": "https://ui-avatars.com/api/?name=Team 15&background=random&length=3"
+    "description": "Seed #15"
   },
   {
     "seed": 16,
-    "code": "t16",
-    "name": "Team 16",
-    "icon": "https://ui-avatars.com/api/?name=Team 16&background=random&length=3"
+    "code": "cag",
+    "name": "CAG Osaka",
+    "icon": "https://img.majors.im/r6/man24/cag.png",
+    "description": "Seed #16"
   }
 ];
 
